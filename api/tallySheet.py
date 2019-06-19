72343574822134adb06565e85c6edab205c6e9e7
dinukadesilva <l.dinukadesilva@gmail.com>
2019-06-19 19:08:43 +0530

    Refactoring

--- api/tallySheet.py
+++ api/tallySheet.py
@@ -3,11 +3,10 @@ This is the people module and supports all the REST actions for the
 people data
 """
 
-from flask import jsonify
 from flask import make_response, abort
 from config import db
-from models import TallySheet, TallySheetVersion, TallySheet_PRE_41
-from schemas import TallySheetSchema, TallySheetVersionSchema, TallySheet_PRE_41_Schema, TallySheet_PRE_41__party_Schema
+from models import TallySheet, TallySheetVersion, TallySheet_PRE_41, TallySheet_PRE_41__party
+from schemas import TallySheetSchema, TallySheetVersionSchema, TallySheet_PRE_41_Schema
 
 
 def getAll():
@@ -25,49 +24,10 @@ def getAll():
 
 
 def get_by_id(tallySheetId):
-    print("################tallySheetId####", tallySheetId)
-
-    # person = Person.query.filter(Person.person_id == person_id).one_or_none()
-
     tallySheet = TallySheet.query.filter(TallySheet.tallySheetId == tallySheetId).one_or_none()
-
-    print("##### tallySheet.latestVersionId ###", tallySheet.latestVersionId)
-
-    # tallySheet_PRE_41 = TallySheetVersion.query. \
-    #     join(TallySheet, TallySheet.id == TallySheetVersion.tallySheetId). \
-    #     join(TallySheet_PRE_41, TallySheet_PRE_41.tallySheetVersionId == TallySheetVersion.id). \
-    #     add_columns(
-    #     TallySheet.id.label("tallySheetId"),
-    #     TallySheet.code.label("code"),
-    #     TallySheetVersion.id.label("tallySheetVersionId"),
-    #     TallySheetVersion.createdBy.label("createdBy"),
-    #     TallySheetVersion.createdAt.label("createdAt"),
-    #     TallySheet_PRE_41.countingCentreId.label("countingCentreId"),
-    #     TallySheet_PRE_41.electoralDistrictId.label("electoralDistrictId"),
-    #     TallySheet_PRE_41.pollingDivisionId.label("pollingDivisionId")
-    # ) \
-    #     .filter(TallySheetVersion.id == tallySheet.latestVersionId).one_or_none()
-
-    # tallySheet_PRE_41 = TallySheet_PRE_41.query. \
-    #     join(TallySheetVersion, TallySheet_PRE_41.tallySheetVersionId == TallySheetVersion.id). \
-    #     join(TallySheet, TallySheet.id == TallySheetVersion.tallySheetId). \
-    #     add_columns(
-    #     TallySheet.id.label("tallySheetId"),
-    #     TallySheet.code.label("code"),
-    #     TallySheetVersion.id.label("tallySheetVersionId"),
-    #     TallySheetVersion.createdBy.label("createdBy"),
-    #     TallySheetVersion.createdAt.label("createdAt"),
-    #     TallySheet_PRE_41.countingCentreId.label("countingCentreId"),
-    #     TallySheet_PRE_41.electoralDistrictId.label("electoralDistrictId"),
-    #     TallySheet_PRE_41.pollingDivisionId.label("pollingDivisionId"),
-    #     TallySheet_PRE_41.party_wise_results.label("party_wise_results")
-    # ) \
-    #     .filter(TallySheetVersion.id == tallySheet.latestVersionId).one_or_none()
 
     tallySheet_PRE_41 = TallySheet_PRE_41.query.filter(
         TallySheet_PRE_41.tallySheetVersionId == tallySheet.latestVersionId).one_or_none()
-
-    # print("#####", tallySheet_PRE_41.__dict__)
 
     person_schema = TallySheet_PRE_41_Schema()
     data = person_schema.dump(tallySheet_PRE_41).data
@@ -75,22 +35,24 @@ def get_by_id(tallySheetId):
 
 
 def create_tallysheet_PRE_41__party(body, tallysheetVersion):
-    schema = TallySheet_PRE_41__party_Schema()
-
     for party_wise_entry_body in body["table"]:
-        new_tallysheet_PRE_41__party = schema.load(party_wise_entry_body, session=db.session).data
-        new_tallysheet_PRE_41__party.tallySheetVersionId = tallysheetVersion.tallySheetVersionId
-        new_tallysheet_PRE_41__party.partyId = party_wise_entry_body["partyId"]
+        new_tallysheet_PRE_41__party = TallySheet_PRE_41__party(
+            partyId=party_wise_entry_body["partyId"],
+            tallySheetVersionId=tallysheetVersion.tallySheetVersionId
+        )
 
         db.session.add(new_tallysheet_PRE_41__party)
         db.session.commit()
 
 
 def create_tallysheet_PRE_41(body, tallysheetVersion):
-    schema = TallySheet_PRE_41_Schema()
-    new_tallysheet_PRE_41 = schema.load(body, session=db.session).data
-    new_tallysheet_PRE_41.tallySheetId = tallysheetVersion.tallySheetId
-    new_tallysheet_PRE_41.tallySheetVersionId = tallysheetVersion.tallySheetVersionId
+    new_tallysheet_PRE_41 = TallySheet_PRE_41(
+        tallySheetId=tallysheetVersion.tallySheetId,
+        tallySheetVersionId=tallysheetVersion.tallySheetVersionId,
+        electoralDistrictId=body["electoralDistrictId"],
+        pollingDivisionId=body["pollingDivisionId"],
+        countingCentreId=body["countingCentreId"],
+    )
 
     db.session.add(new_tallysheet_PRE_41)
     db.session.commit()
@@ -102,14 +64,10 @@ def create_tallysheet_version(body, tallysheet):
     """
         Create new tally sheet version and append it as the latest version.
     """
-
-    # to avoid merging when there's an id passed through the request.
-    body.pop('id', None)
-
-    # Create an instance using the schema and the passed-in data
-    schema = TallySheetVersionSchema()
-    new_tallysheet_version = schema.load(body, session=db.session).data
-    new_tallysheet_version.tallySheetId = tallysheet.tallySheetId
+    new_tallysheet_version = TallySheetVersion(
+        tallySheetId=tallysheet.tallySheetId,
+        createdBy=12  # TODO
+    )
 
     db.session.add(new_tallysheet_version)
     db.session.commit()
