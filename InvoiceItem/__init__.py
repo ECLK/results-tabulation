a1509d018e96f540d95b4e20d207ca86a5f7218f
dinukadesilva <l.dinukadesilva@gmail.com>
2019-07-09 08:34:46 +0530

    Refactoring

--- InvoiceItem/__init__.py
+++ InvoiceItem/__init__.py
@@ -1,16 +1,16 @@
 from flask import abort
 from config import db
-from models import InvoiceModel, InvoiceItemModel
+from models import InvoiceModel, ItemModel
 from schemas import Invoice_Schema, InvoiceItem_Schema
-from domain import InvoiceItemDomain
+from domain import ItemDomain
 
 
 def get_all():
-    invoice_items = InvoiceItemDomain.invoice_item.get_all()
+    invoice_items = ItemDomain.invoice_item.get_all()
     return InvoiceItem_Schema(many=True).dump(invoice_items).data
 
 
 def create(body):
-    invoice_item = InvoiceItemDomain.invoice_item.create(body)
+    invoice_item = ItemDomain.invoice_item.create(body)
 
     return InvoiceItem_Schema().dump(invoice_item).data, 201
