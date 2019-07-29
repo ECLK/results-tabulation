8cedba58f0e495ff5073224584652f65d7f19950
dinukadesilva <l.dinukadesilva@gmail.com>
2019-07-29 12:13:43 +0530

    Correcting the exception titles and types

--- exception/__init__.py
+++ exception/__init__.py
@@ -6,20 +6,20 @@ def UnauthorizedException(message=""):
 
 
 def ForbiddenException(message=""):
-    raise ProblemException(403, "Unauthorized", message, "Unauthorized")
+    raise ProblemException(403, "Forbidden", message, "Forbidden")
 
 
 def NotFoundException(message=""):
-    raise ProblemException(404, "Unauthorized", message, "Unauthorized")
+    raise ProblemException(404, "Not Found", message, "NotFound")
 
 
 def MethodNotAllowedException(message=""):
-    raise ProblemException(405, "Unauthorized", message, "Unauthorized")
+    raise ProblemException(405, "Method Not Allowed", message, "MethodNotAllowed")
 
 
 def InternalServerErrorException(message=""):
-    raise ProblemException(500, "Unauthorized", message, "Unauthorized")
+    raise ProblemException(500, "Internal Server Error", message, "InternalServerError")
 
 
 def NotImplementedException(message=""):
-    raise ProblemException(501, "Unauthorized", message, "Unauthorized")
+    raise ProblemException(501, "Not Implemented", message, "NotImplemented")
