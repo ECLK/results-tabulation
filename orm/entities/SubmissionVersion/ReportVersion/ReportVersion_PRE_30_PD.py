c414db6a3db91bd1753c607f0f2e68fa3b5b83f3
Dinuka De Silva <l.dinukadesilva@gmail.com>
2019-08-21 17:30:17 +0530

    Refactoring

--- orm/entities/SubmissionVersion/ReportVersion/ReportVersion_PRE_30_PD.py
+++ orm/entities/SubmissionVersion/ReportVersion/ReportVersion_PRE_30_PD.py
@@ -44,28 +44,43 @@ class ReportVersion_PRE_30_PD_Model(ReportVersion.Model):
 
         content["countingCentres"] = [countingCentre.areaName for countingCentre in countingCentres]
 
+        # Fill the validVotes, rejectedVotes and totalVotes with zeros.
         for i in range(0, len(countingCentres)):
             content["validVotes"].append(0)
             content["rejectedVotes"].append(0)
             content["totalVotes"].append(0)
 
+        # Iterate by candidates.
         for i in range(int(len(queryResult) / len(countingCentres))):
+            # Append candidate details.
             data_row = [i + 1, queryResult[i].candidateName]
             content["data"].append(data_row)
             total_count_per_candidate = 0
 
+            # Iterate by counting centres.
             for j in range(len(countingCentres)):
+                # Determine the result index mapping with the counting centre and candidate.
                 query_result_index = (i * len(countingCentres)) + j
+
                 count = queryResult[query_result_index].count
+
                 if count is None:
                     data_row.append("")
                 else:
+                    # Append the count of votes of the counting centre.
+                    data_row.append(count)
+
+                    # Calculate the candidate wise total votes.
+                    total_count_per_candidate = total_count_per_candidate + count
+
+                    # Calculate valid votes count.
                     content["validVotes"][j] = content["validVotes"][j] + count
+
+                    # Calculate validVotes count.
                     content["rejectedVotes"][j] = 0  # TODO
+                    
                     content["totalVotes"][j] = content["validVotes"][j] + content["rejectedVotes"][j]
 
-                    data_row.append(count)
-                    total_count_per_candidate = total_count_per_candidate + count
             data_row.append(total_count_per_candidate)
 
         content["validVotes"].append(sum(content["validVotes"]))
