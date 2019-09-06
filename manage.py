0514faed326fffccb883f0e4a518c3decf71bc8d
Kalpa Welivitigoda <callkalpa@gmail.com>
2019-09-07 00:05:39 +0800

    Fix database migration and sample data population issues
    https://github.com/ECLK/results-tabulation-api/issues/58

--- manage.py
+++ manage.py
@@ -1,20 +1,29 @@
 ## FROM https://realpython.com/flask-by-example-part-2-postgres-sqlalchemy-and-alembic/#local-migration
 
 
-import os
-
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
-from app import app, db
+from app import create_app, db
 
-#
 # app.config.from_object(os.environ['APP_SETTINGS'])
 
-migrate = Migrate(app, db)
-manager = Manager(app)
+flask_app = create_app().app
+
+migrate = Migrate(flask_app, db)
+manager = Manager(flask_app)
 
 manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def build_database(dataset):
+    """
+    Populates database with sample dataset.
+    """
+    # """Populates database with sample dataset."""
+    from build_database import build_database
+    build_database(dataset)
 
 
 if __name__ == '__main__':
