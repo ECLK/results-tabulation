cfd59d1ebd7e99fdff63f95a63151d58667c6403
Dinuka De Silva <l.dinukadesilva@gmail.com>
2019-09-10 10:00:53 +0530

    gh-69: Updating the test method naming convensions
    https://www.python.org/dev/peps/pep-0008/#function-and-variable-names

--- tests/api/test_TallySheet.py
+++ tests/api/test_TallySheet.py
@@ -8,49 +8,49 @@ class TestTallySheet:
         assert response.status_code == 200
         assert len(response.get_json()) > 0
 
-    def test_get_all_PRE_ALL_ISLAND_RESULTS_BY_ELECTORAL_DISTRICTS(self, test_client):
+    def test_get_all_pre_all_island_results_by_electoral_districts(self, test_client):
         response: Response = test_client.get(
             "/tally-sheet?tallySheetCode=PRE_ALL_ISLAND_RESULTS_BY_ELECTORAL_DISTRICTS"
         )
         assert response.status_code == 200
         assert len(response.get_json()) > 0
 
-    def test_get_all_PRE_ALL_ISLAND_RESULTS(self, test_client):
+    def test_get_all_pre_all_island_results(self, test_client):
         response: Response = test_client.get(
             "/tally-sheet?tallySheetCode=PRE_ALL_ISLAND_RESULTS"
         )
         assert response.status_code == 200
         assert len(response.get_json()) > 0
 
-    def test_get_all_PRE_30_ED(self, test_client):
+    def test_get_all_pre_30_ed(self, test_client):
         response: Response = test_client.get(
             "/tally-sheet?tallySheetCode=PRE-30-ED"
         )
         assert response.status_code == 200
         assert len(response.get_json()) > 0
 
-    def test_get_all_PRE_30_PD(self, test_client):
+    def test_get_all_pre_30_pd(self, test_client):
         response: Response = test_client.get(
             "/tally-sheet?tallySheetCode=PRE-30-PD"
         )
         assert response.status_code == 200
         assert len(response.get_json()) > 0
 
-    def test_get_all_PRE_41(self, test_client):
+    def test_get_all_pre_41(self, test_client):
         response: Response = test_client.get(
             "/tally-sheet?tallySheetCode=PRE-41"
         )
         assert response.status_code == 200
         assert len(response.get_json()) > 0
 
-    def test_get_all_CE_201(self, test_client):
+    def test_get_all_ce_201(self, test_client):
         response: Response = test_client.get(
             "/tally-sheet?tallySheetCode=CE-201"
         )
         assert response.status_code == 200
         assert len(response.get_json()) > 0
 
-    def test_get_all_PRE_21(self, test_client):
+    def test_get_all_pre_21(self, test_client):
         response: Response = test_client.get(
             "/tally-sheet?tallySheetCode=PRE-21"
         )
