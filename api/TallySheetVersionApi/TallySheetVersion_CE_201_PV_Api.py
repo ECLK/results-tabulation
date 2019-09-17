9f13420513d87a3704ccd02d34449265f186eec7
Dinuka De Silva <l.dinukadesilva@gmail.com>
2019-09-17 16:41:49 +0530

    gh-79: Adding the endpoints and missing properties to CE-201-PV tallysheet
    This closes #79

--- api/TallySheetVersionApi/TallySheetVersion_CE_201_PV_Api.py
+++ api/TallySheetVersionApi/TallySheetVersion_CE_201_PV_Api.py
@@ -1,3 +1,5 @@
+import datetime
+
 from app import db
 from util import RequestBody
 from schemas import TallySheetVersion_CE_201_PV_Schema, TallySheetVersionSchema
@@ -33,14 +35,38 @@ def create(tallySheetId, body):
         tallySheetId=tallySheetId
     )
 
+    total_number_of_a_packets_found = 0
     tally_sheet_content = request_body.get("content")
     if tally_sheet_content is not None:
         for row in tally_sheet_content:
-            party_count_body = RequestBody(row)
-            tallySheetVersion.add_row(
-                count=party_count_body.get("count"),
-                invalidVoteCategoryId=party_count_body.get("invalidVoteCategoryId")
+            tally_sheet_content_item = RequestBody(row)
+            row = tallySheetVersion.add_row(
+                ballotBoxStationaryItemId=tally_sheet_content_item.get("ballotBoxStationaryItemId"),
+                numberOfPacketsInserted=tally_sheet_content_item.get("numberOfPacketsInserted"),
+                numberOfAPacketsFound=tally_sheet_content_item.get("numberOfAPacketsFound")
             )
+
+            total_number_of_a_packets_found = total_number_of_a_packets_found + row.numberOfAPacketsFound
+
+    tally_sheet_summary = request_body.get("summary")
+    time_of_commencement_of_count = tally_sheet_summary.get("timeOfCommencementOfCount")
+
+    # To remove the colon between time zone comming from openapi date-time.
+    # This is since the colon is not accepted in python datetime
+    # if ":" == time_of_commencement_of_count[-3:-2]:
+    #     time_of_commencement_of_count = time_of_commencement_of_count[:-3] + time_of_commencement_of_count[-2:]
+
+    tallySheetVersion.add_summary(
+        situation=tally_sheet_summary.get("situation"),
+        timeOfCommencementOfCount=datetime.datetime.strptime(
+            time_of_commencement_of_count,
+            '%Y-%m-%dT%H:%M:%S.%fZ'
+        ),
+        numberOfAPacketsFound=total_number_of_a_packets_found,
+        numberOfACoversRejected=tally_sheet_summary.get("numberOfACoversRejected"),
+        numberOfBCoversRejected=tally_sheet_summary.get("numberOfBCoversRejected"),
+        numberOfValidBallotPapers=tally_sheet_summary.get("numberOfValidBallotPapers")
+    )
 
     db.session.commit()
 
