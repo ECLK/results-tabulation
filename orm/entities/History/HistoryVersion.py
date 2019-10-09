cc193f5ca41f1f157d3c4bee8fceadf0cb524aba
Dinuka De Silva <l.dinukadesilva@gmail.com>
2019-10-09 11:11:01 +0530

    gh-165: Adding `Stamp` model and link it to history version, proof and files
    This closes #165

--- orm/entities/History/HistoryVersion.py
+++ orm/entities/History/HistoryVersion.py
@@ -1,23 +1,27 @@
-from datetime import datetime
-from util import Auth
 from app import db
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.associationproxy import association_proxy
 
 from orm.entities import History
+from orm.entities.Audit import Stamp
 
 
 class HistoryVersionModel(db.Model):
     __tablename__ = 'history_version'
     historyVersionId = db.Column(db.Integer, primary_key=True, autoincrement=True)
     historyId = db.Column(db.Integer, db.ForeignKey(History.Model.__table__.c.historyId))
-    createdBy = db.Column(db.Integer, nullable=False)
-    createdAt = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now, nullable=False)
+    historyStampId = db.Column(db.Integer, db.ForeignKey(Stamp.Model.__table__.c.stampId), nullable=False)
+
+    historyStamp = relationship(Stamp.Model, foreign_keys=[historyStampId])
+    createdBy = association_proxy("historyStamp", "createdBy")
+    createdAt = association_proxy("historyStamp", "createdAt")
 
     def __init__(self, historyId):
+        history_stamp = Stamp.create()
+
         super(HistoryVersionModel, self).__init__(
             historyId=historyId,
-            createdBy=Auth().get_user_id(),
+            historyStampId=history_stamp.stampId
         )
 
         db.session.add(self)
