d063ede8259759c5a3f12c1bce6fdfc50d911c2b
Umayanga Prabhash Gunawardhana <umayangag@gmail.com>
2019-10-31 21:17:46 +0530

    PRE-34_CO view and edit done

--- orm/entities/SubmissionVersion/TallySheetVersion/TallySheetVersion_PRE_34_CO.py
+++ orm/entities/SubmissionVersion/TallySheetVersion/TallySheetVersion_PRE_34_CO.py
@@ -5,6 +5,7 @@ from app import db
 from orm.entities import Area, Candidate, Party, Election
 from orm.entities.Election import ElectionCandidate
 from orm.entities.SubmissionVersion import TallySheetVersion
+from orm.entities.TallySheetVersionRow import TallySheetVersionRow_PRE_34_preference
 from util import to_comma_seperated_num, sqlalchemy_num_or_zero
 from orm.enums import TallySheetCodeEnum, AreaTypeEnum, VoteTypeEnum
 
@@ -31,8 +32,44 @@ class TallySheetVersion_PRE_34_CO_Model(TallySheetVersion.Model):
             candidateId=candidateId
         )
 
+    @hybrid_property
+    def content(self):
+
+        return db.session.query(
+            ElectionCandidate.Model.candidateId,
+            Candidate.Model.candidateName,
+            Party.Model.partySymbol,
+            TallySheetVersionRow_PRE_34_preference.Model.preferenceNumber,
+            TallySheetVersionRow_PRE_34_preference.Model.preferenceCount,
+            TallySheetVersionRow_PRE_34_preference.Model.tallySheetVersionId,
+            TallySheetVersionRow_PRE_34_preference.Model.electionId
+        ).join(
+            TallySheetVersionRow_PRE_34_preference.Model,
+            and_(
+                TallySheetVersionRow_PRE_34_preference.Model.candidateId == ElectionCandidate.Model.candidateId,
+                TallySheetVersionRow_PRE_34_preference.Model.tallySheetVersionId == self.tallySheetVersionId,
+            ),
+            isouter=True
+        ).join(
+            Candidate.Model,
+            Candidate.Model.candidateId == ElectionCandidate.Model.candidateId,
+            isouter=True
+        ).join(
+            Party.Model,
+            Party.Model.partyId == ElectionCandidate.Model.partyId,
+            isouter=True
+        ).filter(
+            ElectionCandidate.Model.electionId.in_(self.submission.election.mappedElectionIds),
+            ElectionCandidate.Model.qualifiedForPreferences == True
+        ).all()
+
     def html(self):
         stamp = self.stamp
+
+        polling_divisions = Area.get_associated_areas(self.submission.area, AreaTypeEnum.PollingDivision)
+        polling_division_name = ""
+        if len(polling_divisions) > 0:
+            polling_division_name = polling_divisions[0].areaName
 
         content = {
             "election": {
@@ -43,87 +80,39 @@ class TallySheetVersion_PRE_34_CO_Model(TallySheetVersion.Model):
                 "createdBy": stamp.createdBy,
                 "barcodeString": stamp.barcodeString
             },
-            "electoralDistrict": self.submission.area.areaName,
-            "pollingDivisions": [],
-            "data": [],
-            "validVoteCounts": [],
-            "rejectedVoteCounts": [],
-            "totalVoteCounts": []
+            "title": "PRESIDENTIAL ELECTION ACT NO. 15 OF 1981",
+            "electoralDistrict": Area.get_associated_areas(
+                self.submission.area, AreaTypeEnum.ElectoralDistrict)[0].areaName,
+            "pollingDivision": polling_division_name,
+            "countingCentre": self.submission.area.areaName,
+            "pollingDistrictNos": ", ".join([
+                pollingDistrict.areaName for pollingDistrict in
+                Area.get_associated_areas(self.submission.area, AreaTypeEnum.PollingDistrict)
+            ]),
+            "data": [
+            ],
         }
 
-        non_postal_candidate_and_area_wise_valid_vote_count_result = self.non_postal_candidate_and_area_wise_valid_vote_count_query().all()
-        postal_candidate_and_area_wise_valid_vote_count_result = self.postal_candidate_and_area_wise_valid_vote_count_query().all()
-        candidate_wise_vote_count_result = self.candidate_wise_vote_count().all()
-        non_postal_area_wise_vote_count_result = self.non_postal_area_wise_vote_count_query().all()
-        postal_area_wise_vote_count_result = self.postal_area_wise_vote_count_query().all()
-        vote_count_result = self.vote_count_query().one_or_none()
-
-        polling_division_count = len(non_postal_area_wise_vote_count_result)
-        postal_polling_division_count = len(postal_area_wise_vote_count_result)
-        number_of_candidates = len(candidate_wise_vote_count_result)
-
-        for polling_division_index in range(polling_division_count):
-            non_postal_area_wise_vote_count_result_item = non_postal_area_wise_vote_count_result[
-                polling_division_index]
-            content["pollingDivisions"].append(non_postal_area_wise_vote_count_result_item.areaName)
-            content["rejectedVoteCounts"].append(
-                to_comma_seperated_num(non_postal_area_wise_vote_count_result_item.rejectedVoteCount)
-            )
-            content["validVoteCounts"].append(
-                to_comma_seperated_num(non_postal_area_wise_vote_count_result_item.validVoteCount)
-            )
-            content["totalVoteCounts"].append(
-                to_comma_seperated_num(non_postal_area_wise_vote_count_result_item.totalVoteCount)
-            )
-
-        for postal_polling_division_index in range(postal_polling_division_count):
-            postal_area_wise_vote_count_result_item = postal_area_wise_vote_count_result[
-                postal_polling_division_index]
-            content["rejectedVoteCounts"].append(
-                to_comma_seperated_num(postal_area_wise_vote_count_result_item.rejectedVoteCount)
-            )
-            content["validVoteCounts"].append(
-                to_comma_seperated_num(postal_area_wise_vote_count_result_item.validVoteCount)
-            )
-            content["totalVoteCounts"].append(
-                to_comma_seperated_num(postal_area_wise_vote_count_result_item.totalVoteCount)
-            )
-
-        content["rejectedVoteCounts"].append(to_comma_seperated_num(vote_count_result.rejectedVoteCount))
-        content["validVoteCounts"].append(to_comma_seperated_num(vote_count_result.validVoteCount))
-        content["totalVoteCounts"].append(to_comma_seperated_num(vote_count_result.totalVoteCount))
-
-        for candidate_wise_vote_count_result_item_index in range(number_of_candidates):
-            data_row = []
-
-            data_row_number = candidate_wise_vote_count_result_item_index + 1
-            data_row.append(data_row_number)
-
-            candidate_wise_vote_count_result_item = candidate_wise_vote_count_result[
-                candidate_wise_vote_count_result_item_index
-            ]
-            data_row.append(candidate_wise_vote_count_result_item.candidateName)
-
-            for polling_division_index in range(polling_division_count):
-                non_postal_area_wise_valid_vote_count_result_item_index = (
-                                                                                  candidate_wise_vote_count_result_item_index *
-                                                                                  polling_division_count) + polling_division_index
-                non_postal_candidate_and_area_wise_valid_vote_count_result_item = \
-                    non_postal_candidate_and_area_wise_valid_vote_count_result[
-                        non_postal_area_wise_valid_vote_count_result_item_index
-                    ]
-                data_row.append(to_comma_seperated_num(
-                    non_postal_candidate_and_area_wise_valid_vote_count_result_item.validVoteCount))
-
-            postal_candidate_and_area_wise_valid_vote_count_result_item = \
-                postal_candidate_and_area_wise_valid_vote_count_result[
-                    candidate_wise_vote_count_result_item_index
-                ]
-            data_row.append(
-                to_comma_seperated_num(postal_candidate_and_area_wise_valid_vote_count_result_item.validVoteCount))
-            data_row.append(to_comma_seperated_num(candidate_wise_vote_count_result_item.validVoteCount))
-
-            content["data"].append(data_row)
+        # for row_index in range(len(tallySheetContent)):
+        #     row = tallySheetContent[row_index]
+        #     if row.count is not None:
+        #         content["data"].append([
+        #             row_index + 1,
+        #             row.candidateName,
+        #             row.partySymbol,
+        #             row.preferenceNumber,
+        #             to_comma_seperated_num(row.count),
+        #             ""
+        #         ])
+        #     else:
+        #         content["data"].append([
+        #             row_index + 1,
+        #             row.candidateName,
+        #             row.partySymbol,
+        #             "",
+        #             "",
+        #             ""
+        #         ])
 
         html = render_template(
             'PRE-34-CO.html',
