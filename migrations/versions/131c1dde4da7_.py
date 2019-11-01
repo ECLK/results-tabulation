a252d5498b93f6ad18afe1c51400268bec6792e0
Umayanga Prabhash Gunawardhana <umayangag@gmail.com>
2019-11-01 20:43:44 +0530

    add migrations

---
+++ migrations/versions/131c1dde4da7_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 131c1dde4da7
+Revises: 1e1babe0eff8, 689cea37ffd9
+Create Date: 2019-11-01 19:59:03.790646
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '131c1dde4da7'
+down_revision = ('1e1babe0eff8', '689cea37ffd9')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
