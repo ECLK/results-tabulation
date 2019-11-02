a588546ad93024bb91e4b5fd915e4b603cc00506
Dinuka De Silva <l.dinukadesilva@gmail.com>
2019-11-02 17:10:20 +0530

    Update the exceptions

--- exception/__init__.py
+++ exception/__init__.py
@@ -2,24 +2,24 @@ from connexion import ProblemException
 
 
 def UnauthorizedException(message="", code=None):
-    raise ProblemException(401, "Unauthorized", message, "Unauthorized")
+    raise ProblemException(401, "Unauthorized", message, "Unauthorized", code)
 
 
 def ForbiddenException(message="", code=None):
-    raise ProblemException(403, "Forbidden", message, "Forbidden")
+    raise ProblemException(403, "Forbidden", message, "Forbidden", code)
 
 
 def NotFoundException(message="", code=None):
-    raise ProblemException(404, "Not Found", message, "NotFound")
+    raise ProblemException(404, "Not Found", message, "NotFound", code)
 
 
 def MethodNotAllowedException(message="", code=None):
-    raise ProblemException(405, "Method Not Allowed", message, "MethodNotAllowed")
+    raise ProblemException(405, "Method Not Allowed", message, "MethodNotAllowed", code)
 
 
 def InternalServerErrorException(message="", code=None):
-    raise ProblemException(500, "Internal Server Error", message, "InternalServerError")
+    raise ProblemException(500, "Internal Server Error", message, "InternalServerError", code)
 
 
 def NotImplementedException(message="", code=None):
-    raise ProblemException(501, "Not Implemented", message, "NotImplemented")
+    raise ProblemException(501, "Not Implemented", message, "NotImplemented", code)
