3f8d59be84846a90ec94b9a87b58de65133c2dc3
Kalpa Welivitigoda <callkalpa@gmail.com>
2019-11-02 13:31:29 +0800

    [#279] Modify required roles in tally sheet version APIs

--- api/TallySheetVersionApi/TallySheetVersionPRE41Api.py
+++ api/TallySheetVersionApi/TallySheetVersionPRE41Api.py
@@ -1,5 +1,5 @@
 from app import db
-from auth import authorize, EC_LEADERSHIP_ROLE
+from auth import authorize, EC_LEADERSHIP_ROLE, POLLING_DIVISION_REPORT_VERIFIER_ROLE
 from auth.AuthConstants import DATA_EDITOR_ROLE
 from exception import NotFoundException
 from orm.entities.Submission import TallySheet
@@ -9,7 +9,7 @@ from schemas import TallySheetVersionPRE41Schema, TallySheetVersionSchema
 from util import RequestBody
 
 
-@authorize(required_roles=[DATA_EDITOR_ROLE, EC_LEADERSHIP_ROLE])
+@authorize(required_roles=[DATA_EDITOR_ROLE, POLLING_DIVISION_REPORT_VERIFIER_ROLE, EC_LEADERSHIP_ROLE])
 def get_by_id(tallySheetId, tallySheetVersionId):
     tallySheet = TallySheet.get_by_id(tallySheetId=tallySheetId)
     if tallySheet is None:
--- api/TallySheetVersionApi/TallySheetVersion_CE_201_PV_Api.py
+++ api/TallySheetVersionApi/TallySheetVersion_CE_201_PV_Api.py
@@ -1,6 +1,6 @@
 import datetime
 from app import db
-from auth import authorize
+from auth import authorize, POLLING_DIVISION_REPORT_VERIFIER_ROLE
 from auth.AuthConstants import DATA_EDITOR_ROLE, EC_LEADERSHIP_ROLE
 from orm.entities.SubmissionVersion import TallySheetVersion
 from orm.enums import TallySheetCodeEnum
@@ -10,7 +10,7 @@ from orm.entities.Submission import TallySheet
 from exception import NotFoundException
 
 
-@authorize(required_roles=[DATA_EDITOR_ROLE, EC_LEADERSHIP_ROLE])
+@authorize(required_roles=[DATA_EDITOR_ROLE, POLLING_DIVISION_REPORT_VERIFIER_ROLE, EC_LEADERSHIP_ROLE])
 def get_by_id(tallySheetId, tallySheetVersionId):
     result = TallySheetVersion.get_by_id(
         tallySheetId=tallySheetId,
