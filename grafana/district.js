fc657db584878ebfef10aadc365a2dc8d77f24cb
Mohamed Nizar <nizarucsc@gmail.com>
2019-11-12 22:25:36 +0530

    Added RO suffix

--- grafana/district.js
+++ grafana/district.js
@@ -62,70 +62,70 @@ return function (callback) {
             debugger;
             var district = null
                 switch (user){
-                    case 'colombo':
+                    case 'colombo-ro':
                         district = '01 - Colombo';
                         break;
-                    case 'gampaha':
+                    case 'gampaha-ro':
                         district = '02 - Gampaha';
                         break;
-                    case 'kalutara':
+                    case 'kalutara-ro':
                         district = '03 - Kalutara';
                         break;
-                    case 'kandy':
+                    case 'kandy-ro':
                         district = '04 - Kandy';
                         break;
-                    case 'matale':
+                    case 'matale-ro':
                         district = '05 - Matale';
                         break;
-                    case 'nuwaraeliya':
+                    case 'nuwaraeliya-ro':
                         district = '06 - Nuwara Eliya';
                         break;
-                    case 'galle':
+                    case 'galle-ro':
                         district = '07 - Galle';
                         break;
-                    case 'matara':
+                    case 'matara-ro':
                         district = '08 - Matara';
                         break;
-                    case 'hambantota':
+                    case 'hambantota-ro':
                         district = '09 - Hambantota';
                         break;
-                    case 'jaffna':
+                    case 'jaffna-ro':
                         district = '10 - Jaffna';
                         break;
-                    case 'vanni':
+                    case 'vanni-ro':
                         district = '11 - Vanni';
                         break;
-                    case 'batticaloa':
+                    case 'batticaloa-ro':
                         district = '12 - Batticaloa';
                         break;
-                    case 'digamdulla':
+                    case 'digamdulla-ro':
                         district = '13 - Digamdulla';
                         break;
-                    case 'trincomalee':
+                    case 'trincomalee-ro':
                         district = '14 - Trincomalee';
                         break;
-                    case 'krunegala':
+                    case 'krunegala-ro':
                         district = '15 - Krunegala';
                         break;
-                    case 'puttalam':
+                    case 'puttalam-ro':
                         district = '16 - Puttalam';
                         break;
-                    case 'anuradhapura':
+                    case 'anuradhapura-ro':
                         district = '17 - Anuradhapura';
                         break;
-                    case 'polannaruwa':
+                    case 'polannaruwa-ro':
                         district = '18 - Polannaruwa';
                         break;
-                    case 'badulla':
+                    case 'badulla-ro':
                         district = '19 - Badulla';
                         break;
-                    case 'monaragala':
+                    case 'monaragala-ro':
                         district = '20 - monaragala';
                         break;
-                    case 'ratnapura':
+                    case 'ratnapura-ro':
                         district = '21 - Ratnapura';
                         break;
-                    case 'kegalle':
+                    case 'kegalle-ro':
                         district = '22 - Kegalle';
                         break;
                 }
