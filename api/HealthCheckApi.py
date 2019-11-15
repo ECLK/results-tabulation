b195eb6c5d60bba008d0ebc3f0dd77d552978f85
Umayanga <umayangag@gmail.com>
2019-11-15 19:52:16 +0530

    remove unused imports

--- api/HealthCheckApi.py
+++ api/HealthCheckApi.py
@@ -1,9 +1,2 @@
-from util import RequestBody, get_area_type, get_paginated_query
-
-from schemas import AreaSchema as Schema
-from orm.entities import Area
-import connexion
-
-
 def get():
     return "Health Check Success!", 200
