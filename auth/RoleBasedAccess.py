5d9f42536f61a94c9b85c204459a0d2bb57293ca
Dinuka De Silva <l.dinukadesilva@gmail.com>
2019-11-19 10:37:46 +0530

    Disable write access temporary

--- auth/RoleBasedAccess.py
+++ auth/RoleBasedAccess.py
@@ -78,76 +78,76 @@ role_to_read_allowed_tallysheet_types = {
 }
 
 role_to_write_allowed_tallysheet_types = {
-    DATA_EDITOR_ROLE: [
-        TallySheetCodeEnum.PRE_41,
-        TallySheetCodeEnum.CE_201,
-        TallySheetCodeEnum.CE_201_PV,
-        TallySheetCodeEnum.PRE_34_CO
-    ]
+    # DATA_EDITOR_ROLE: [
+    #     TallySheetCodeEnum.PRE_41,
+    #     TallySheetCodeEnum.CE_201,
+    #     TallySheetCodeEnum.CE_201_PV,
+    #     TallySheetCodeEnum.PRE_34_CO
+    # ]
 }
 
 role_to_lock_allowed_tallysheet_types = {
-    DATA_EDITOR_ROLE: [
-        TallySheetCodeEnum.PRE_41,
-        TallySheetCodeEnum.CE_201,
-        TallySheetCodeEnum.CE_201_PV,
-        TallySheetCodeEnum.PRE_34_CO
-    ],
-    POLLING_DIVISION_REPORT_VERIFIER_ROLE: [
-        TallySheetCodeEnum.PRE_30_PD,
-        TallySheetCodeEnum.PRE_34_I_RO,
-        TallySheetCodeEnum.PRE_34_PD
-    ],
-    ELECTORAL_DISTRICT_REPORT_VERIFIER_ROLE: [
-        TallySheetCodeEnum.PRE_30_PD,
-        TallySheetCodeEnum.PRE_30_ED,
-        TallySheetCodeEnum.PRE_34_I_RO,
-        TallySheetCodeEnum.PRE_34_II_RO,
-        TallySheetCodeEnum.PRE_34,
-        TallySheetCodeEnum.PRE_34_PD,
-        TallySheetCodeEnum.PRE_34_ED
-    ],
-    NATIONAL_REPORT_VERIFIER_ROLE: [
-        TallySheetCodeEnum.PRE_ALL_ISLAND_RESULTS,
-        TallySheetCodeEnum.PRE_ALL_ISLAND_RESULTS_BY_ELECTORAL_DISTRICTS,
-        TallySheetCodeEnum.PRE_34_AI
-    ]
+    # DATA_EDITOR_ROLE: [
+    #     TallySheetCodeEnum.PRE_41,
+    #     TallySheetCodeEnum.CE_201,
+    #     TallySheetCodeEnum.CE_201_PV,
+    #     TallySheetCodeEnum.PRE_34_CO
+    # ],
+    # POLLING_DIVISION_REPORT_VERIFIER_ROLE: [
+    #     TallySheetCodeEnum.PRE_30_PD,
+    #     TallySheetCodeEnum.PRE_34_I_RO,
+    #     TallySheetCodeEnum.PRE_34_PD
+    # ],
+    # ELECTORAL_DISTRICT_REPORT_VERIFIER_ROLE: [
+    #     TallySheetCodeEnum.PRE_30_PD,
+    #     TallySheetCodeEnum.PRE_30_ED,
+    #     TallySheetCodeEnum.PRE_34_I_RO,
+    #     TallySheetCodeEnum.PRE_34_II_RO,
+    #     TallySheetCodeEnum.PRE_34,
+    #     TallySheetCodeEnum.PRE_34_PD,
+    #     TallySheetCodeEnum.PRE_34_ED
+    # ],
+    # NATIONAL_REPORT_VERIFIER_ROLE: [
+    #     TallySheetCodeEnum.PRE_ALL_ISLAND_RESULTS,
+    #     TallySheetCodeEnum.PRE_ALL_ISLAND_RESULTS_BY_ELECTORAL_DISTRICTS,
+    #     TallySheetCodeEnum.PRE_34_AI
+    # ]
 }
 
 role_to_unlock_allowed_tallysheet_types = {
-    POLLING_DIVISION_REPORT_VERIFIER_ROLE: [
-        TallySheetCodeEnum.PRE_41,
-        TallySheetCodeEnum.CE_201,
-        TallySheetCodeEnum.CE_201_PV,
-        TallySheetCodeEnum.PRE_34_CO
-    ],
-    ELECTORAL_DISTRICT_REPORT_VERIFIER_ROLE: [
-        TallySheetCodeEnum.PRE_41,
-        TallySheetCodeEnum.CE_201_PV,
-        TallySheetCodeEnum.PRE_34_CO,
-    ],
-    NATIONAL_REPORT_VERIFIER_ROLE: [
-        TallySheetCodeEnum.PRE_30_PD,
-        TallySheetCodeEnum.PRE_30_ED,
-        TallySheetCodeEnum.PRE_34_I_RO,
-        TallySheetCodeEnum.PRE_34_II_RO,
-        TallySheetCodeEnum.PRE_34,
-        # TallySheetCodeEnum.PRE_ALL_ISLAND_RESULTS,
-        # TallySheetCodeEnum.PRE_ALL_ISLAND_RESULTS_BY_ELECTORAL_DISTRICTS,
-        TallySheetCodeEnum.PRE_34_PD,
-        TallySheetCodeEnum.PRE_34_ED,
-        TallySheetCodeEnum.PRE_34_AI
-    ],
-    EC_LEADERSHIP_ROLE: [
-        TallySheetCodeEnum.PRE_30_PD,
-        TallySheetCodeEnum.PRE_30_ED,
-        TallySheetCodeEnum.PRE_34_I_RO,
-        TallySheetCodeEnum.PRE_34_II_RO,
-        TallySheetCodeEnum.PRE_34,
-        TallySheetCodeEnum.PRE_ALL_ISLAND_RESULTS,
-        TallySheetCodeEnum.PRE_ALL_ISLAND_RESULTS_BY_ELECTORAL_DISTRICTS,
-        TallySheetCodeEnum.PRE_34_PD,
-        TallySheetCodeEnum.PRE_34_ED,
-        TallySheetCodeEnum.PRE_34_AI
-    ]
+    # POLLING_DIVISION_REPORT_VERIFIER_ROLE: [
+    #     TallySheetCodeEnum.PRE_41,
+    #     TallySheetCodeEnum.CE_201,
+    #     TallySheetCodeEnum.CE_201_PV,
+    #     TallySheetCodeEnum.PRE_34_CO
+    # ],
+    # ELECTORAL_DISTRICT_REPORT_VERIFIER_ROLE: [
+    #     TallySheetCodeEnum.PRE_41,
+    #     TallySheetCodeEnum.CE_201_PV,
+    #     TallySheetCodeEnum.PRE_34_CO,
+    # ],
+    # NATIONAL_REPORT_VERIFIER_ROLE: [
+    #     TallySheetCodeEnum.PRE_30_PD,
+    #     TallySheetCodeEnum.PRE_30_ED,
+    #     TallySheetCodeEnum.PRE_34_I_RO,
+    #     TallySheetCodeEnum.PRE_34_II_RO,
+    #     TallySheetCodeEnum.PRE_34,
+    #     # TallySheetCodeEnum.PRE_ALL_ISLAND_RESULTS,
+    #     # TallySheetCodeEnum.PRE_ALL_ISLAND_RESULTS_BY_ELECTORAL_DISTRICTS,
+    #     TallySheetCodeEnum.PRE_34_PD,
+    #     TallySheetCodeEnum.PRE_34_ED,
+    #     TallySheetCodeEnum.PRE_34_AI
+    # ],
+    # EC_LEADERSHIP_ROLE: [
+    #     TallySheetCodeEnum.PRE_30_PD,
+    #     TallySheetCodeEnum.PRE_30_ED,
+    #     TallySheetCodeEnum.PRE_34_I_RO,
+    #     TallySheetCodeEnum.PRE_34_II_RO,
+    #     TallySheetCodeEnum.PRE_34,
+    #     TallySheetCodeEnum.PRE_ALL_ISLAND_RESULTS,
+    #     TallySheetCodeEnum.PRE_ALL_ISLAND_RESULTS_BY_ELECTORAL_DISTRICTS,
+    #     TallySheetCodeEnum.PRE_34_PD,
+    #     TallySheetCodeEnum.PRE_34_ED,
+    #     TallySheetCodeEnum.PRE_34_AI
+    # ]
 }
