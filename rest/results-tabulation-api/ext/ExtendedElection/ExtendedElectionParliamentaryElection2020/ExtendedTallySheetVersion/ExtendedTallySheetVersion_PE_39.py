5b9149076aa0762c736e7be4dc0bbf8e69be923d
Kalpa Welivitigoda <callkalpa@gmail.com>
2020-03-06 22:33:52 +0800

    [#476] Add PE-39 report

--- rest/results-tabulation-api/ext/ExtendedElection/ExtendedElectionParliamentaryElection2020/ExtendedTallySheetVersion/ExtendedTallySheetVersion_PE_39.py
+++ rest/results-tabulation-api/ext/ExtendedElection/ExtendedElectionParliamentaryElection2020/ExtendedTallySheetVersion/ExtendedTallySheetVersion_PE_39.py
@@ -10,11 +10,58 @@ class ExtendedTallySheetVersion_PE_39(ExtendedTallySheetVersion):
 
     def __init__(self, tallySheetVersion):
         super(ExtendedTallySheetVersion_PE_39, self).__init__(tallySheetVersion)
+
     #
     # def html_letter(self, title="", total_registered_voters=None):
     #     # TODO: implement
     #     pass
     #
-    # def html(self, title="", total_registered_voters=None):
-    #     # TODO: implement
-    #     pass
+    def html(self, title="", total_registered_voters=None):
+        tallySheetVersion = self.tallySheetVersion
+
+        invalid_vote_category_counts = self.get_invalid_vote_category_count()
+
+        stamp = tallySheetVersion.stamp
+
+        polling_divisions = Area.get_associated_areas(tallySheetVersion.submission.area, AreaTypeEnum.PollingDivision)
+        polling_division_name = ""
+        if len(polling_divisions) > 0:
+            polling_division_name = polling_divisions[0].areaName
+
+        if tallySheetVersion.submission.election.voteType == Postal:
+            polling_division_name = 'Postal'
+
+        content = {
+            "election": {
+                "electionName": tallySheetVersion.submission.election.get_official_name()
+            },
+            "stamp": {
+                "createdAt": stamp.createdAt,
+                "createdBy": stamp.createdBy,
+                "barcodeString": stamp.barcodeString
+            },
+            "tallySheetCode": "PE-39",
+            "electoralDistrict": Area.get_associated_areas(
+                tallySheetVersion.submission.area, AreaTypeEnum.ElectoralDistrict)[0].areaName,
+            "pollingDivision": polling_division_name,
+            "countingCentre": tallySheetVersion.submission.area.areaName,
+            "data": [],
+            "rejectedVotes": 0
+        }
+
+        total_rejected_count = 0
+        for index, invalid_vote_category_count in invalid_vote_category_counts.iterrows():
+            data_row = []
+            data_row.append(invalid_vote_category_count.invalidVoteCategoryDescription)
+            data_row.append(invalid_vote_category_count.numValue)
+            content["data"].append(data_row)
+            total_rejected_count += invalid_vote_category_count.numValue
+
+        content["rejectedVotes"] = to_comma_seperated_num(total_rejected_count)
+
+        html = render_template(
+            'PE-39.html',
+            content=content
+        )
+
+        return html
