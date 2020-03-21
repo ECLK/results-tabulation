5b8db38a99c72ec70303d9002a500d9d3520b760
Dinuka De Silva <L.dinukadesilva@gmail.com>
2020-03-22 05:26:19 +0530

    gh-502: Refactoring

--- rest/results-tabulation-api/orm/entities/Audit/Barcode.py
+++ rest/results-tabulation-api/orm/entities/Audit/Barcode.py
@@ -8,23 +8,24 @@ class BarcodeModel(db.Model):
     barcodeId = db.Column(db.Integer, primary_key=True, autoincrement=True)
     barcodeString = db.Column(db.String(13), nullable=True)
 
+    @classmethod
+    def _get_barcode_string(cls, num):
+        num_str = str(num)
+        for i in range(BARCODE_LENGTH - len(num_str)):
+            num_str = "0" + num_str
+        return num_str
+
+    @classmethod
+    def create(cls):
+        barcode = cls()
+        db.session.add(barcode)
+        db.session.flush()
+
+        barcode.barcodeString = cls._get_barcode_string(barcode.barcodeId)
+        db.session.flush()
+
+        return barcode
+
 
 Model = BarcodeModel
-
-
-def _get_barcode_string(num):
-    num_str = str(num)
-    for i in range(BARCODE_LENGTH - len(num_str)):
-        num_str = "0" + num_str
-    return num_str
-
-
-def create():
-    barcode = BarcodeModel()
-    db.session.add(barcode)
-    db.session.flush()
-
-    barcode.barcodeString = _get_barcode_string(barcode.barcodeId)
-    db.session.flush()
-
-    return barcode
+create = Model.create
