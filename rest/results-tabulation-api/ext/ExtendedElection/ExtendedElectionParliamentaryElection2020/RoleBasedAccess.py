6909ac31573109ca7875fb720df5398a9ca2be8b
Dinuka De Silva <L.dinukadesilva@gmail.com>
2020-04-06 16:05:40 +0530

    gh-536: Enabling print option for all the roles having read access in PE

--- rest/results-tabulation-api/ext/ExtendedElection/ExtendedElectionParliamentaryElection2020/RoleBasedAccess.py
+++ rest/results-tabulation-api/ext/ExtendedElection/ExtendedElectionParliamentaryElection2020/RoleBasedAccess.py
@@ -8,9 +8,10 @@ from ext.ExtendedElection.ExtendedElectionParliamentaryElection2020.TALLY_SHEET_
 from ext.ExtendedElection.ExtendedElectionParliamentaryElection2020.WORKFLOW_ACTION_TYPE import \
     WORKFLOW_ACTION_TYPE_SAVE, WORKFLOW_ACTION_TYPE_VIEW, WORKFLOW_ACTION_TYPE_SUBMIT, WORKFLOW_ACTION_TYPE_VERIFY, \
     WORKFLOW_ACTION_TYPE_REQUEST_CHANGES, WORKFLOW_ACTION_TYPE_MOVE_TO_CERTIFY, WORKFLOW_ACTION_TYPE_CERTIFY, \
-    WORKFLOW_ACTION_TYPE_RELEASE, WORKFLOW_ACTION_TYPE_EDIT
+    WORKFLOW_ACTION_TYPE_RELEASE, WORKFLOW_ACTION_TYPE_EDIT, WORKFLOW_ACTION_TYPE_PRINT
 
 READ = WORKFLOW_ACTION_TYPE_VIEW
+PRINT = WORKFLOW_ACTION_TYPE_PRINT
 WRITE = WORKFLOW_ACTION_TYPE_SAVE
 SUBMIT = WORKFLOW_ACTION_TYPE_SUBMIT
 EDIT = WORKFLOW_ACTION_TYPE_EDIT
@@ -23,146 +24,146 @@ RELEASE = WORKFLOW_ACTION_TYPE_RELEASE
 role_based_access_config = {
     DATA_EDITOR_ROLE: {
         PE_27: {
-            NonPostal: [READ, WRITE, SUBMIT, EDIT, LOCK],
-            Postal: [READ, WRITE, SUBMIT, EDIT, LOCK]
+            NonPostal: [READ, PRINT, WRITE, SUBMIT, EDIT, LOCK],
+            Postal: [READ, PRINT, WRITE, SUBMIT, EDIT, LOCK]
         },
         PE_39: {
-            NonPostal: [READ, WRITE, SUBMIT, EDIT, LOCK],
-            Postal: [READ, WRITE, SUBMIT, EDIT, LOCK]
+            NonPostal: [READ, PRINT, WRITE, SUBMIT, EDIT, LOCK],
+            Postal: [READ, PRINT, WRITE, SUBMIT, EDIT, LOCK]
         },
         PE_22: {
-            NonPostal: [READ, WRITE, SUBMIT, EDIT, LOCK],
-            Postal: [READ, WRITE, SUBMIT, EDIT, LOCK]
+            NonPostal: [READ, PRINT, WRITE, SUBMIT, EDIT, LOCK],
+            Postal: [READ, PRINT, WRITE, SUBMIT, EDIT, LOCK]
         },
         CE_201: {
-            NonPostal: [READ, WRITE, SUBMIT, EDIT, LOCK]
+            NonPostal: [READ, PRINT, WRITE, SUBMIT, EDIT, LOCK]
         },
         CE_201_PV: {
-            Postal: [READ, WRITE, SUBMIT, EDIT, LOCK]
+            Postal: [READ, PRINT, WRITE, SUBMIT, EDIT, LOCK]
         },
         PE_4: {
-            NonPostal: [READ, WRITE, SUBMIT, EDIT, LOCK],
-            Postal: [READ, WRITE, SUBMIT, EDIT, LOCK]
+            NonPostal: [READ, PRINT, WRITE, SUBMIT, EDIT, LOCK],
+            Postal: [READ, PRINT, WRITE, SUBMIT, EDIT, LOCK]
         },
     },
     POLLING_DIVISION_REPORT_VIEWER_ROLE: {
         PE_CE_RO_V1: {
-            NonPostal: [READ, WRITE]
+            NonPostal: [READ, PRINT, WRITE]
         },
         POLLING_DIVISION_RESULTS: {
-            NonPostal: [READ, WRITE]
+            NonPostal: [READ, PRINT, WRITE]
         },
         PE_R1: {
-            NonPostal: [READ, WRITE]
+            NonPostal: [READ, PRINT, WRITE]
         },
         PE_CE_RO_PR_1: {
-            NonPostal: [READ, WRITE]
+            NonPostal: [READ, PRINT, WRITE]
         }
     },
     POLLING_DIVISION_REPORT_VERIFIER_ROLE: {
         PE_27: {
-            NonPostal: [READ, UNLOCK]
+            NonPostal: [READ, PRINT, UNLOCK]
         },
         PE_39: {
-            NonPostal: [READ, UNLOCK]
+            NonPostal: [READ, PRINT, UNLOCK]
         },
         PE_22: {
-            NonPostal: [READ, UNLOCK]
+            NonPostal: [READ, PRINT, UNLOCK]
         },
         CE_201: {
-            NonPostal: [READ, UNLOCK]
+            NonPostal: [READ, PRINT, UNLOCK]
         },
         PE_4: {
-            NonPostal: [READ, UNLOCK]
+            NonPostal: [READ, PRINT, UNLOCK]
         },
         PE_CE_RO_V1: {
-            NonPostal: [READ, WRITE, LOCK]
+            NonPostal: [READ, PRINT, WRITE, LOCK]
         },
         POLLING_DIVISION_RESULTS: {
-            NonPostal: [READ, WRITE, LOCK]
+            NonPostal: [READ, PRINT, WRITE, LOCK]
         },
         PE_R1: {
-            NonPostal: [READ, WRITE, LOCK]
+            NonPostal: [READ, PRINT, WRITE, LOCK]
         },
         PE_CE_RO_PR_1: {
-            NonPostal: [READ, WRITE, LOCK]
+            NonPostal: [READ, PRINT, WRITE, LOCK]
         }
     },
     ELECTORAL_DISTRICT_REPORT_VIEWER_ROLE: {
         PE_CE_RO_V1: {
-            Postal: [READ, WRITE]
+            Postal: [READ, PRINT, WRITE]
         },
         POLLING_DIVISION_RESULTS: {
-            Postal: [READ, WRITE]
+            Postal: [READ, PRINT, WRITE]
         },
         PE_R1: {
-            Postal: [READ, WRITE]
+            Postal: [READ, PRINT, WRITE]
         },
         PE_CE_RO_PR_1: {
-            Postal: [READ, WRITE]
+            Postal: [READ, PRINT, WRITE]
         },
         PE_CE_RO_V2: {
-            PostalAndNonPostal: [READ, WRITE]
+            PostalAndNonPostal: [READ, PRINT, WRITE]
         },
         PE_R2: {
-            PostalAndNonPostal: [READ, WRITE]
+            PostalAndNonPostal: [READ, PRINT, WRITE]
         },
         PE_CE_RO_PR_2: {
-            PostalAndNonPostal: [READ, WRITE]
+            PostalAndNonPostal: [READ, PRINT, WRITE]
         },
         PE_CE_RO_PR_3: {
-            PostalAndNonPostal: [READ, WRITE]
+            PostalAndNonPostal: [READ, PRINT, WRITE]
         },
         PE_21: {
-            PostalAndNonPostal: [READ, WRITE]
+            PostalAndNonPostal: [READ, PRINT, WRITE]
         }
     },
     ELECTORAL_DISTRICT_REPORT_VERIFIER_ROLE: {
         PE_27: {
-            Postal: [READ, UNLOCK]
+            Postal: [READ, PRINT, UNLOCK]
         },
         PE_39: {
-            Postal: [READ, UNLOCK]
+            Postal: [READ, PRINT, UNLOCK]
         },
         PE_22: {
-            Postal: [READ, UNLOCK]
+            Postal: [READ, PRINT, UNLOCK]
         },
         CE_201_PV: {
-            Postal: [READ, UNLOCK]
+            Postal: [READ, PRINT, UNLOCK]
         },
         PE_4: {
-            Postal: [READ, UNLOCK]
+            Postal: [READ, PRINT, UNLOCK]
         },
         PE_CE_RO_V1: {
-            Postal: [READ, WRITE, LOCK],
-            NonPostal: [READ, WRITE, UNLOCK]
+            Postal: [READ, PRINT, WRITE, LOCK],
+            NonPostal: [READ, PRINT, WRITE, UNLOCK]
         },
         POLLING_DIVISION_RESULTS: {
-            Postal: [READ, WRITE, LOCK],
-            NonPostal: [READ, WRITE, UNLOCK]
+            Postal: [READ, PRINT, WRITE, LOCK],
+            NonPostal: [READ, PRINT, WRITE, UNLOCK]
         },
         PE_R1: {
-            Postal: [READ, WRITE, LOCK],
-            NonPostal: [READ, WRITE, UNLOCK]
+            Postal: [READ, PRINT, WRITE, LOCK],
+            NonPostal: [READ, PRINT, WRITE, UNLOCK]
         },
         PE_CE_RO_PR_1: {
-            Postal: [READ, WRITE, LOCK],
-            NonPostal: [READ, WRITE, UNLOCK]
+            Postal: [READ, PRINT, WRITE, LOCK],
+            NonPostal: [READ, PRINT, WRITE, UNLOCK]
         },
         PE_CE_RO_V2: {
-            PostalAndNonPostal: [READ, WRITE, LOCK]
+            PostalAndNonPostal: [READ, PRINT, WRITE, LOCK]
         },
         PE_R2: {
-            PostalAndNonPostal: [READ, WRITE, LOCK]
+            PostalAndNonPostal: [READ, PRINT, WRITE, LOCK]
         },
         PE_CE_RO_PR_2: {
-            PostalAndNonPostal: [READ, WRITE, LOCK]
+            PostalAndNonPostal: [READ, PRINT, WRITE, LOCK]
         },
         PE_CE_RO_PR_3: {
-            PostalAndNonPostal: [READ, WRITE, LOCK]
+            PostalAndNonPostal: [READ, PRINT, WRITE, LOCK]
         },
         PE_21: {
-            PostalAndNonPostal: [READ, WRITE, LOCK]
+            PostalAndNonPostal: [READ, PRINT, WRITE, LOCK]
         }
     },
     NATIONAL_REPORT_VIEWER_ROLE: {
@@ -170,68 +171,68 @@ role_based_access_config = {
     },
     NATIONAL_REPORT_VERIFIER_ROLE: {
         PE_R2: {
-            PostalAndNonPostal: [READ, WRITE, UNLOCK]
+            PostalAndNonPostal: [READ, PRINT, WRITE, UNLOCK]
         },
         PE_CE_RO_PR_3: {
-            PostalAndNonPostal: [READ, WRITE, UNLOCK]
+            PostalAndNonPostal: [READ, PRINT, WRITE, UNLOCK]
         },
         PE_21: {
-            PostalAndNonPostal: [READ, WRITE, UNLOCK]
+            PostalAndNonPostal: [READ, PRINT, WRITE, UNLOCK]
         }
     },
     EC_LEADERSHIP_ROLE: {
         PE_27: {
-            Postal: [READ, UNLOCK],
-            NonPostal: [READ, UNLOCK]
+            Postal: [READ, PRINT, UNLOCK],
+            NonPostal: [READ, PRINT, UNLOCK]
         },
         PE_39: {
-            Postal: [READ, UNLOCK],
-            NonPostal: [READ, UNLOCK]
+            Postal: [READ, PRINT, UNLOCK],
+            NonPostal: [READ, PRINT, UNLOCK]
         },
         PE_22: {
-            Postal: [READ, UNLOCK],
-            NonPostal: [READ, UNLOCK]
+            Postal: [READ, PRINT, UNLOCK],
+            NonPostal: [READ, PRINT, UNLOCK]
         },
         CE_201_PV: {
-            Postal: [READ, UNLOCK]
+            Postal: [READ, PRINT, UNLOCK]
         },
         CE_201: {
-            NonPostal: [READ, UNLOCK]
+            NonPostal: [READ, PRINT, UNLOCK]
         },
         PE_4: {
-            Postal: [READ, UNLOCK],
-            NonPostal: [READ, UNLOCK]
+            Postal: [READ, PRINT, UNLOCK],
+            NonPostal: [READ, PRINT, UNLOCK]
         },
         PE_CE_RO_V1: {
-            Postal: [READ, WRITE, UNLOCK, MOVE_TO_CERTIFY, CERTIFY, RELEASE],
-            NonPostal: [READ, WRITE, UNLOCK, MOVE_TO_CERTIFY, CERTIFY, RELEASE]
+            Postal: [READ, PRINT, WRITE, UNLOCK, MOVE_TO_CERTIFY, CERTIFY, RELEASE],
+            NonPostal: [READ, PRINT, WRITE, UNLOCK, MOVE_TO_CERTIFY, CERTIFY, RELEASE]
         },
         POLLING_DIVISION_RESULTS: {
-            Postal: [READ, WRITE, UNLOCK, MOVE_TO_CERTIFY, CERTIFY, RELEASE],
-            NonPostal: [READ, WRITE, UNLOCK, MOVE_TO_CERTIFY, CERTIFY, RELEASE]
+            Postal: [READ, PRINT, WRITE, UNLOCK, MOVE_TO_CERTIFY, CERTIFY, RELEASE],
+            NonPostal: [READ, PRINT, WRITE, UNLOCK, MOVE_TO_CERTIFY, CERTIFY, RELEASE]
         },
         PE_R1: {
-            Postal: [READ, WRITE, UNLOCK, MOVE_TO_CERTIFY, CERTIFY, RELEASE],
-            NonPostal: [READ, WRITE, UNLOCK, MOVE_TO_CERTIFY, CERTIFY, RELEASE]
+            Postal: [READ, PRINT, WRITE, UNLOCK, MOVE_TO_CERTIFY, CERTIFY, RELEASE],
+            NonPostal: [READ, PRINT, WRITE, UNLOCK, MOVE_TO_CERTIFY, CERTIFY, RELEASE]
         },
         PE_CE_RO_V2: {
-            PostalAndNonPostal: [READ, WRITE, UNLOCK, MOVE_TO_CERTIFY, CERTIFY, RELEASE]
+            PostalAndNonPostal: [READ, PRINT, WRITE, UNLOCK, MOVE_TO_CERTIFY, CERTIFY, RELEASE]
         },
         PE_R2: {
-            PostalAndNonPostal: [READ, WRITE, UNLOCK, MOVE_TO_CERTIFY, CERTIFY, RELEASE]
+            PostalAndNonPostal: [READ, PRINT, WRITE, UNLOCK, MOVE_TO_CERTIFY, CERTIFY, RELEASE]
         },
         PE_CE_RO_PR_1: {
-            Postal: [READ, WRITE, UNLOCK],
-            NonPostal: [READ, WRITE, UNLOCK]
+            Postal: [READ, PRINT, WRITE, UNLOCK],
+            NonPostal: [READ, PRINT, WRITE, UNLOCK]
         },
         PE_CE_RO_PR_2: {
-            PostalAndNonPostal: [READ, WRITE, UNLOCK]
+            PostalAndNonPostal: [READ, PRINT, WRITE, UNLOCK]
         },
         PE_CE_RO_PR_3: {
-            PostalAndNonPostal: [READ, WRITE, UNLOCK, MOVE_TO_CERTIFY, CERTIFY, RELEASE]
+            PostalAndNonPostal: [READ, PRINT, WRITE, UNLOCK, MOVE_TO_CERTIFY, CERTIFY, RELEASE]
         },
         PE_21: {
-            PostalAndNonPostal: [READ, WRITE, UNLOCK, MOVE_TO_CERTIFY, CERTIFY, RELEASE]
+            PostalAndNonPostal: [READ, PRINT, WRITE, UNLOCK, MOVE_TO_CERTIFY, CERTIFY, RELEASE]
         }
     }
 }
