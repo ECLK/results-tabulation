015de1f2cad701bf020a5f9ee7c0e2e9567288c1
Dinuka De Silva <L.dinukadesilva@gmail.com>
2020-04-23 17:56:38 +0530

    gh-519: Adding the env REACT_APP_DEBUG for enabling debug on front end.

--- web/results-tabulation-ui/src/config.js
+++ web/results-tabulation-ui/src/config.js
@@ -1,5 +1,6 @@
 export let AUTH_APP_URL = "http://localhost:3001";
 export let TABULATION_API_URL = 'http://localhost:5000';
+export let DEBUG = false;
 
 if (process.env.REACT_APP_AUTH_APP_URL) {
     AUTH_APP_URL = process.env.REACT_APP_AUTH_APP_URL;
@@ -7,4 +8,8 @@ if (process.env.REACT_APP_AUTH_APP_URL) {
 
 if (process.env.REACT_APP_TABULATION_API_URL) {
     TABULATION_API_URL = process.env.REACT_APP_TABULATION_API_URL;
+}
+
+if (process.env.REACT_APP_DEBUG) {
+    DEBUG = process.env.REACT_APP_DEBUG;
 }
