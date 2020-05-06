dacd805f1257d09e662aea6a1aad1cbb79fb9d4c
Dinuka De Silva <L.dinukadesilva@gmail.com>
2020-05-07 03:18:21 +0530

    gh-571: Improve styles of error page

--- web/results-tabulation-ui/src/components/error.js
+++ web/results-tabulation-ui/src/components/error.js
@@ -1,16 +1,29 @@
 import React from "react";
+import WarningIcon from '@material-ui/icons/Warning';
 
 
 export default function Error(
     {
         title,
-        body = "This could be because you are trying to access a resource you aren't authorized."
+        body
     }
 ) {
-    return <div className="tabulation-page-message">
-        <strong>{title}</strong>
-        <p>{body}</p>
+    if (!body) {
+        body = <>
+            This could be because you are trying to access a resource you aren't authorized.
+            <br/><small>Check your internet connection.</small>
+        </>
+    }
 
-        <small>Check your internet connection.</small>
+    if (!title) {
+        title = "[Error] Unknown error."
+    }
+
+    return <div className="tabulation-page-message">
+        <WarningIcon style={{fontSize: 55, padding: "0px 40px", color: "#FFC107"}}/>
+        <div className="tabulation-page-message-content">
+            <strong>{title}</strong>
+            <p>{body}</p>
+        </div>
     </div>
 }
