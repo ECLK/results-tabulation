168621112c4a1848a30a87549c5724926543cc8a
Dinuka De Silva <L.dinukadesilva@gmail.com>
2020-06-09 14:16:22 +0530

    gh-634: Append the candidateMap to parties on election partyMap creation
    This fixes #634

--- results-tabulation-ui/src/services/util.js
+++ results-tabulation-ui/src/services/util.js
@@ -12,9 +12,22 @@ export function getPartyMap(partyList) {
     const partyMap = {};
     for (let i = 0; i < partyList.length; i++) {
         const party = partyList[i];
-        const {partyId} = party;
+        const {partyId, candidates} = party;
+        party.candidateMap = getCandidateMap(candidates);
+
         partyMap[partyId] = party;
     }
 
     return partyMap;
+}
+
+export function getCandidateMap(candidateList) {
+    const candidateMap = {};
+    for (let i = 0; i < candidateList.length; i++) {
+        const candidate = candidateList[i];
+        const {candidateId} = candidate;
+        candidateMap[candidateId] = candidate;
+    }
+
+    return candidateMap;
 }
