ac938d073c061a1ad19abbf3e3570d9c765036c7
Dinuka De Silva <L.dinukadesilva@gmail.com>
2020-07-01 11:37:52 +0530

    gh-577: Error handling pdf-service external requests

--- results-tabulation-api/external_services/pdf_service/__init__.py
+++ results-tabulation-api/external_services/pdf_service/__init__.py
@@ -3,27 +3,46 @@ from exception.messages import MESSAGE_CODE_PDF_SERVICE_ENTRY_CREATION_FAILED, M
 from exception import InternalServerErrorException
 import requests
 import json
+import traceback
 
 
 def html_to_pdf(html):
-    pdf_service_entry_response = requests.request(
-        method="POST",
-        url="%s/generate" % connex_app.app.config['PDF_SERVICE_URL'],
-        headers={'Content-Type': 'application/json'},
-        data=json.dumps({"html": html})
-    )
+    try:
+        pdf_service_entry_response = requests.request(
+            method="POST",
+            url="%s/generate" % connex_app.app.config['PDF_SERVICE_URL'],
+            headers={'Content-Type': 'application/json'},
+            data=json.dumps({"html": html})
+        )
+    except Exception as e:
+        error_string = traceback.format_exc()
+        print(error_string)
 
-    if pdf_service_entry_response.status_code != 200:
         raise InternalServerErrorException(
-            message="PDF creation failed.",
+            message="PDF creation request failed.",
             code=MESSAGE_CODE_PDF_SERVICE_ENTRY_CREATION_FAILED
         )
 
-    pdf_response = requests.get(url=pdf_service_entry_response.json()["url"])
+    if pdf_service_entry_response.status_code != 200:
+        raise InternalServerErrorException(
+            message="PDF creation unsuccessful.",
+            code=MESSAGE_CODE_PDF_SERVICE_ENTRY_CREATION_FAILED
+        )
+
+    try:
+        pdf_response = requests.get(url=pdf_service_entry_response.json()["url"])
+    except Exception as e:
+        error_string = traceback.format_exc()
+        print(error_string)
+
+        raise InternalServerErrorException(
+            message="PDF fetch request failed.",
+            code=MESSAGE_CODE_PDF_SERVICE_FETCH_FAILED
+        )
 
     if pdf_response.status_code != 200:
         raise InternalServerErrorException(
-            message="PDF fetch failed.",
+            message="PDF fetch unsuccessful.",
             code=MESSAGE_CODE_PDF_SERVICE_FETCH_FAILED
         )
 
