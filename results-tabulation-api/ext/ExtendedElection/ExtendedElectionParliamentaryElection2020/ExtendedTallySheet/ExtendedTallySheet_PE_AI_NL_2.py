c172d04abd94e18fa26570721b5a68023531a035
Dinuka De Silva <L.dinukadesilva@gmail.com>
2020-07-23 02:23:56 +0530

    gh-700: Adding the report template for PE-AI-NL-2

--- results-tabulation-api/ext/ExtendedElection/ExtendedElectionParliamentaryElection2020/ExtendedTallySheet/ExtendedTallySheet_PE_AI_NL_2.py
+++ results-tabulation-api/ext/ExtendedElection/ExtendedElectionParliamentaryElection2020/ExtendedTallySheet/ExtendedTallySheet_PE_AI_NL_2.py
@@ -18,68 +18,6 @@ from util import convert_image_to_data_uri
 
 
 class ExtendedTallySheet_PE_AI_NL_2(ExtendedEditableTallySheetReport):
-    # def get_template_column_to_query_filter_map(self, only_group_by_columns=False):
-    #     extended_election = self.tallySheet.election.get_extended_election()
-    #
-    #     template_column_to_query_filter_map = super(
-    #         ExtendedTallySheet_PE_AI_NL_2, self).get_template_column_to_query_filter_map(
-    #         only_group_by_columns=only_group_by_columns)
-    #     template_column_to_query_column_map = self.get_template_column_to_query_column_map()
-    #
-    #     party_ids_to_be_filtered = []
-    #     pe_ai_nl_1_tally_sheets = db.session.query(TallySheet.Model).filter(
-    #         TallySheet.Model.tallySheetId == TallySheetTallySheetModel.childTallySheetId,
-    #         TallySheet.Model.tallySheetId == Submission.Model.submissionId,
-    #         Submission.Model.latestVersionId != None,
-    #         TallySheetTallySheetModel.parentTallySheetId == self.tallySheet.tallySheetId,
-    #         TallySheet.Model.templateId == Template.Model.templateId,
-    #         Template.Model.templateName == TALLY_SHEET_CODES.PE_AI_NL_1,
-    #         WorkflowInstance.Model.workflowInstanceId == TallySheet.Model.workflowInstanceId,
-    #         WorkflowInstance.Model.status.in_(
-    #             extended_election.tally_sheet_verified_statuses_list()
-    #         ),
-    #     ).all()
-    #
-    #     if len(pe_ai_nl_1_tally_sheets) == 0:
-    #         raise ForbiddenException(
-    #             message="National list candidates cannot be allocated until the national list seat calculation (PE-AI-NL-1) is completed and verified.",
-    #             code=MESSAGE_CODE_PE_21_CANNOT_BE_PROCESSED_WITHOUT_PE_R2
-    #         )
-    #
-    #     pe_ai_nl_1_tally_sheet_ids = [tallySheet.tallySheetId for tallySheet in pe_ai_nl_1_tally_sheets]
-    #
-    #     for pe_ai_nl_1_tally_sheet in pe_ai_nl_1_tally_sheets:
-    #         pe_ai_nl_1_extended_tally_sheet_version = pe_ai_nl_1_tally_sheet.get_extended_tally_sheet_version(
-    #             tallySheetVersionId=pe_ai_nl_1_tally_sheet.latestVersionId)
-    #         party_wise_seat_calculation_df = pe_ai_nl_1_extended_tally_sheet_version.get_party_wise_seat_calculations()
-    #         for party_wise_seat_calculation_df_index in party_wise_seat_calculation_df.index:
-    #             seats_allocated = party_wise_seat_calculation_df.at[
-    #                 party_wise_seat_calculation_df_index, 'seatsAllocated']
-    #
-    #             if seats_allocated > 0:
-    #                 party_id = party_wise_seat_calculation_df.at[party_wise_seat_calculation_df_index, 'partyId']
-    #                 party_ids_to_be_filtered.append(int(party_id))
-    #
-    #     pe_ce_ro_pr_3_tally_sheets = db.session.query(
-    #         TallySheet.Model.tallySheetId
-    #     ).filter(
-    #         TallySheet.Model.tallySheetId == TallySheetTallySheetModel.childTallySheetId,
-    #         TallySheetTallySheetModel.parentTallySheetId == self.tallySheet.tallySheetId,
-    #         TallySheet.Model.templateId == Template.Model.templateId,
-    #         Template.Model.templateName == TALLY_SHEET_CODES.PE_CE_RO_PR_3,
-    #         MetaData.Model.metaId == TallySheet.Model.metaId,
-    #         MetaData.Model.metaDataKey == "partyId",
-    #         MetaData.Model.metaDataValue.in_(party_ids_to_be_filtered)
-    #     ).all()
-    #     pe_ce_ro_pr_3_tally_sheet_ids = [tallySheet.tallySheetId for tallySheet in pe_ce_ro_pr_3_tally_sheets]
-    #
-    #     template_column_to_query_filter_map["partyId"] += [
-    #         template_column_to_query_column_map["partyId"].in_(party_ids_to_be_filtered),
-    #         TallySheet.Model.tallySheetId.in_(pe_ce_ro_pr_3_tally_sheet_ids + pe_r2_tally_sheet_ids)
-    #     ]
-    #
-    #     return template_column_to_query_filter_map
-
     class ExtendedTallySheetVersion(ExtendedEditableTallySheetReport.ExtendedTallySheetVersion):
 
         def get_post_save_request_content(self):
@@ -157,13 +95,10 @@ class ExtendedTallySheet_PE_AI_NL_2(ExtendedEditableTallySheetReport):
                     "createdBy": stamp.createdBy,
                     "barcodeString": stamp.barcodeString
                 },
-                "tallySheetCode": "PE-21",
-                "electoralDistrictNo": Area.get_associated_areas(
-                    tallySheetVersion.submission.area, AreaTypeEnum.ElectoralDistrict)[0].areaId,
-                "electoralDistrict": Area.get_associated_areas(
-                    tallySheetVersion.submission.area, AreaTypeEnum.ElectoralDistrict)[0].areaName,
-                "countingCentre": tallySheetVersion.submission.area.areaName,
-                "data": []
+                "data": [],
+                "logo": convert_image_to_data_uri("static/Emblem_of_Sri_Lanka.png"),
+                "date": stamp.createdAt.strftime("%d/%m/%Y"),
+                "time": stamp.createdAt.strftime("%H:%M:%S %p")
             }
 
             elected_candidates_df = self.df.loc[
@@ -174,60 +109,19 @@ class ExtendedTallySheet_PE_AI_NL_2(ExtendedEditableTallySheetReport):
             )
 
             for index in elected_candidates_df.index:
-                candidate_name = elected_candidates_df.at[index, "candidateName"]
                 party_name = elected_candidates_df.at[index, "partyName"]
-                content["data"].append({
-                    "candidateName": "" if candidate_name is None else candidate_name,
-                    "partyName": party_name
-                })
+                party_abbreviation = elected_candidates_df.at[index, "partyAbbreviation"]
+                candidate_number = elected_candidates_df.at[index, "candidateNumber"]
+                candidate_name = elected_candidates_df.at[index, "candidateName"]
+                content["data"].append([
+                    party_name,
+                    party_abbreviation,
+                    "" if candidate_number is None else candidate_number,
+                    "" if candidate_name is None else candidate_name
+                ])
 
             html = render_template(
-                'PE-21.html',
-                content=content
-            )
-
-            return html
-
-        def html_letter(self, title="", total_registered_voters=None):
-            tallySheetVersion = self.tallySheetVersion
-            stamp = tallySheetVersion.stamp
-
-            content = {
-                "election": {
-                    "electionName": tallySheetVersion.submission.election.get_official_name()
-                },
-                "stamp": {
-                    "createdAt": stamp.createdAt,
-                    "createdBy": stamp.createdBy,
-                    "barcodeString": stamp.barcodeString
-                },
-                "electoralDistrict": Area.get_associated_areas(
-                    tallySheetVersion.submission.area, AreaTypeEnum.ElectoralDistrict)[0].areaName,
-                "data": [],
-                "logo": convert_image_to_data_uri("static/Emblem_of_Sri_Lanka.png"),
-                "date": stamp.createdAt.strftime("%d/%m/%Y"),
-                "time": stamp.createdAt.strftime("%H:%M:%S %p")
-            }
-
-            elected_candidates_df = self.df.loc[
-                (self.df['templateRowType'] == TEMPLATE_ROW_TYPE_ELECTED_CANDIDATE) & (self.df['numValue'] == 0)]
-
-            for index in elected_candidates_df.index:
-                candidateId = elected_candidates_df.at[index, "candidateId"]
-                preference_count_df = self.df.loc[(self.df['templateRowType'] == "CANDIDATE_FIRST_PREFERENCE") & (
-                        self.df['candidateId'] == candidateId)]
-
-                data_row = [
-                    elected_candidates_df.at[index, "partyName"],
-                    elected_candidates_df.at[index, "partyAbbreviation"],
-                    elected_candidates_df.at[index, "candidateName"],
-                    elected_candidates_df.at[index, "candidateNumber"]
-                ]
-
-                content["data"].append(data_row)
-
-            html = render_template(
-                'ParliamentaryElection2020/PE-21-LETTER.html',
+                'ParliamentaryElection2020/PE-AI-NL-2.html',
                 content=content
             )
 
