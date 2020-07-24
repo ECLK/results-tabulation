3a1b37dca176c62857ae50932949632851ee18f2
Dinuka De Silva <L.dinukadesilva@gmail.com>
2020-07-25 03:41:30 +0530

    Refactoring

--- results-tabulation-ui/src/components/bread-crumb.js
+++ results-tabulation-ui/src/components/bread-crumb.js
@@ -3,8 +3,8 @@ import {Link} from 'react-router-dom';
 
 export default function BreadCrumb({links}) {
     return <ul className="bread-crumb">
-        {links.map((link) => {
-            return <li key={link.to} className="bread-crumb-list-item">
+        {links.map((link, linkIndex) => {
+            return <li key={linkIndex} className="bread-crumb-list-item">
                 <Link to={link.to}>{link.label} </Link> /
             </li>
         })}
