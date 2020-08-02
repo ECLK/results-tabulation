7f70c36140c95d73ab825aa592230f6777e4e070
Dinuka De Silva <L.dinukadesilva@gmail.com>
2020-08-02 12:14:10 +0530

    gh-735: Adding the letter template for PE-CE-RO-V2

--- results-tabulation-api/ext/ExtendedElection/ExtendedElectionParliamentaryElection2020/ExtendedTallySheet/ExtendedTallySheet_PE_CE_RO_V2.py
+++ results-tabulation-api/ext/ExtendedElection/ExtendedElectionParliamentaryElection2020/ExtendedTallySheet/ExtendedTallySheet_PE_CE_RO_V2.py
@@ -64,9 +64,84 @@ class ExtendedTallySheet_PE_CE_RO_V2(ExtendedTallySheetReport):
             }
 
         def html_letter(self, title="", total_registered_voters=None):
-            return super(ExtendedTallySheet_PE_CE_RO_V2.ExtendedTallySheetVersion, self).html_letter(
-                title="Results of Electoral District %s" % self.tallySheetVersion.submission.area.areaName
+            tallySheetVersion = self.tallySheetVersion
+            party_wise_valid_vote_count_result = self.get_party_wise_valid_vote_count_result()
+            area_wise_valid_vote_count_result = self.get_area_wise_valid_vote_count_result()
+            area_wise_rejected_vote_count_result = self.get_area_wise_rejected_vote_count_result()
+            area_wise_vote_count_result = self.get_area_wise_vote_count_result()
+            stamp = tallySheetVersion.stamp
+
+            registered_voters_count = tallySheetVersion.submission.area.get_registered_voters_count(
+                vote_type=tallySheetVersion.submission.election.voteType)
+
+            content = {
+                "election": {
+                    "electionName": tallySheetVersion.submission.election.get_official_name()
+                },
+                "stamp": {
+                    "createdAt": stamp.createdAt,
+                    "createdBy": stamp.createdBy,
+                    "barcodeString": stamp.barcodeString
+                },
+                "electoralDistrict": tallySheetVersion.submission.area.areaName,
+                "data": [],
+                "validVoteCounts": [0, "0%"],
+                "rejectedVoteCounts": [0, "0%"],
+                "totalVoteCounts": [0, "0%"],
+                "registeredVoters": [
+                    to_comma_seperated_num(registered_voters_count),
+                    100
+                ],
+                "logo": convert_image_to_data_uri("static/Emblem_of_Sri_Lanka.png"),
+                "date": stamp.createdAt.strftime("%d/%m/%Y"),
+                "time": stamp.createdAt.strftime("%H:%M:%S %p")
+            }
+
+            total_valid_vote_count = 0
+            for area_wise_valid_vote_count_result_item in area_wise_valid_vote_count_result.itertuples():
+                total_valid_vote_count += float(area_wise_valid_vote_count_result_item.incompleteNumValue)
+            content["validVoteCounts"][0] = to_comma_seperated_num(total_valid_vote_count)
+            content["validVoteCounts"][1] = to_percentage((total_valid_vote_count / registered_voters_count) * 100)
+
+            total_rejected_vote_count = 0
+            for area_wise_rejected_vote_count_result_item in area_wise_rejected_vote_count_result.itertuples():
+                total_rejected_vote_count += float(area_wise_rejected_vote_count_result_item.numValue)
+            content["rejectedVoteCounts"][0] = to_comma_seperated_num(total_rejected_vote_count)
+            content["rejectedVoteCounts"][1] = to_percentage(
+                (total_rejected_vote_count / registered_voters_count) * 100)
+
+            total_vote_count = 0
+            for area_wise_vote_count_result_item in area_wise_vote_count_result.itertuples():
+                total_vote_count += float(area_wise_vote_count_result_item.incompleteNumValue)
+            content["totalVoteCounts"][0] = to_comma_seperated_num(total_vote_count)
+            content["totalVoteCounts"][1] = to_percentage((total_vote_count / registered_voters_count) * 100)
+
+            # sort by vote count descending
+            party_wise_valid_vote_count_result = party_wise_valid_vote_count_result.sort_values(
+                by=['numValue', "electionPartyId"], ascending=False
+            ).reset_index()
+
+            for party_wise_valid_vote_count_result_item_index, party_wise_valid_vote_count_result_item in party_wise_valid_vote_count_result.iterrows():
+                data_row = [
+                    party_wise_valid_vote_count_result_item.partyName,
+                    party_wise_valid_vote_count_result_item.partyAbbreviation,
+                    to_comma_seperated_num(party_wise_valid_vote_count_result_item.numValue)
+                ]
+
+                if total_valid_vote_count > 0:
+                    data_row.append(to_percentage(
+                        party_wise_valid_vote_count_result_item.numValue * 100 / total_valid_vote_count))
+                else:
+                    data_row.append('')
+
+                content["data"].append(data_row)
+
+            html = render_template(
+                'ParliamentaryElection2020/PE-CE-RO-V2-LETTER.html',
+                content=content
             )
+
+            return html
 
         def html(self, title="", total_registered_voters=None):
             tallySheetVersion = self.tallySheetVersion
